4d6706aa48ddcdc9c7f3f31037e4ba2e62cc6824
Đình Duy Võ <duy7210@gmail.com>
2023-03-15 17:11:04 +0700

    finish error page

--- src/main/java/com/diduy/Application/Controller/ErrorPageController.java
+++ src/main/java/com/diduy/Application/Controller/ErrorPageController.java
@@ -3,12 +3,30 @@ package com.diduy.Application;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.RequestDispatcher;
+
 
 @Controller
 public class ErrorPageController implements ErrorController {
 
   @RequestMapping("/error")
-  public String handleError() {
-    return "error";
+  public String handleError(HttpServletRequest request) {
+
+    Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+    if(status != null) {
+      Integer statusCode = Integer.valueOf(status.toString());
+
+      if (statusCode == HttpStatus.NOT_FOUND.value()) {
+        return "error-404";
+      }
+      else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+        return "error-500";
+      }
   }
+
+  return "error";
+}
 }
